3b10a6c50081156a0a9b22e86be9aa86ae013d53
nealpatel <nealpatel2021@gmail.com>
2020-07-21 11:56:41 -0400

    daily easy/medium

--- 2020/07/21/sort_list.py
+++ 2020/07/21/sort_list.py
@@ -19,6 +19,7 @@ def construct_LL(array):
 
 	return head
 
+# 392 ms (14.88%), 27 MB (6.88%)
 def sort_list(head):
 	# convert to unsorted array -> O(n)
 	# sort unsorted array -> O(n log n) ==> merge/quicksort
@@ -73,6 +74,71 @@ def sort_list(head):
 	print(sorted_array)
 
 	return sorted_LL
+
+# 340 ms (23.85%), 47.4 MB (5.09%)
+def sort_list_merge_sort_for_linked_lists(head):
+
+	def merge_sort_helper(left_node, right_node):
+		resulting_list = None
+		if not left_node: return right_node
+		if not right_node: return left_node
+
+		if left_node.val <= right_node.val:
+			resulting_list = left_node
+			resulting_list.next = merge_sort_helper(left_node.next, right_node)
+		else:
+			resulting_list = right_node
+			resulting_list.next = merge_sort_helper(left_node, right_node.next)
+		return resulting_list
+
+	def get_middle_of_linked_list(head):
+		if head == None:
+			return head
+
+		slow = fast = head
+		while fast.next != None and fast.next.next != None:
+			slow = slow.next
+			fast = fast.next.next
+
+		return slow
+
+	if head == None or head.next == None:
+		return head
+
+	middle_node = get_middle_of_linked_list(head)
+	middle_node_next = middle_node.next
+
+	middle_node.next = None
+
+	left_node = sort_list_merge_sort_for_linked_lists(head)
+	right_node = sort_list_merge_sort_for_linked_lists(middle_node_next)
+
+	return merge_sort_helper(left_node, right_node)
+
+# 240 ms (66.53%), 23 MB (59.31%)
+def sort_list_merge_sort_better(head):
+	def merge(left_node, right_node):
+		dummy = tail = ListNode(None)
+		while left_node and right_node:
+			if left_node.val < right_node.val:
+				tail.next, tail, left_node = left_node, left_node, left_node.next
+			else:
+				tail.next, tail, right_node = right_node, right_node, right_node.next
+
+		tail.next = left_node or right_node # the one extra one
+
+		return dummy.next
+
+	if not head or not head.next:
+		return head
+
+	pre, slow, fast = None, head, head
+	while fast and fast.next:
+		pre, slow, fast = slow, slow.next, fast.next.next
+	pre.next = None # curtails to make 2 lists seperated at the middle
+
+	return merge(*map(sort_list_merge_sort_better, (head, slow)))
+
 
 
 
